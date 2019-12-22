1fe11f30bd3b144db04fe06e0c8bf88746c49fb2
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-12-22 14:21:31 +0900

    Update version to v1.0.5

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ const (
 	ghUser     string = "GITHUB_USER"
 	ghToken    string = "GITHUB_TOKEN"
 	urlPattern string = "https://%s:%s@github.com/%s/%s.git"
-	version    string = "v1.0.4"
+	version    string = "v1.0.5"
 )
 
 var (
